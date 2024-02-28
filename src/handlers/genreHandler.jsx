94b06bc498733b9c4104408efa6623cff9dab3c3
kwetso-ogodiseng-kgaladi1 <kgaladikwetso@gmail.com>
2024-02-28 07:49:31 +0200

    added genres for the podcasts

---
+++ src/handlers/genreHandler.jsx
@@ -0,0 +1,11 @@
+export const genreList = {
+  1: 'Personal Growth', // Genre ID 1 corresponds to 'Personal Growth'
+  2: 'Investigative Journalism', // Genre ID 2 corresponds to 'Investigative Journalism'
+  3: 'History', // Genre ID 3 corresponds to 'History'
+  4: 'Comedy', // Genre ID 4 corresponds to 'Comedy'
+  5: 'Entertainment', // Genre ID 5 corresponds to 'Entertainment'
+  6: 'Business', // Genre ID 6 corresponds to 'Business'
+  7: 'Fiction', // Genre ID 7 corresponds to 'Fiction'
+  8: 'News', // Genre ID 8 corresponds to 'News'
+  9: 'Kids and Family', // Genre ID 9 corresponds to 'Kids and Family'
+};
